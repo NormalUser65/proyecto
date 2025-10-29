52e33c1a954e3f58f93829a9c338a405fe49730f
Fabricio Arias <faariasza@est.utn.ac.cr>
2025-10-29 14:33:44 -0600

    Corrección de modelos y controladores

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 // Composer autoloader
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 /*Encabezada de las solicitudes*/
 /*CORS*/
 header("Access-Control-Allow-Origin: * ");
